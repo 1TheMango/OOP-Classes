47e25018057697a369d8b59a34b079e873d5a7e4
1TheMango <ammarrehman0@outlook.com>
2025-09-02 12:47:06 +0500

    yessir

--- dynamicmem.cpp
+++ dynamicmem.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 
-    int* new_int = new int; //dyanmic memory allocation
+    /*int* new_int = new int; //dyanmic memory allocation
     *new_int= 5;
     delete new_int; // memoryleaks
     new_int = nullptr; //dangling pointer
@@ -12,17 +12,39 @@ int main(){
     cin>>size;
     int* new_array =new int[size];
 
-    /*
+    /
     do whatever
-    */
+    
    delete[] new_array;
    new_array;
+   */
 
    struct list{
     int size =0;
     int* arr = nullptr;
-    int* new_int = new int[size+1]
+    
+    void add(int x){
+        size+=1;
+        int* new_arr =new int[size];
+        for(int i=0; i<(size-1); i++){
+            new_arr[i]=arr[i];
+        }
+        new_arr[size-1]=x;
+        delete[] arr;
+        arr= new_arr;
+        //new_arr= nullptr;
+    };
 
-
+   };
+   list newlist;
+   newlist.add(4);
+   newlist.add(56);
+   for(int i=0;i<newlist.size; i++){
+    cout<<newlist.arr[i]<<endl;
    }
-}
+
+
+
+
+
+};
